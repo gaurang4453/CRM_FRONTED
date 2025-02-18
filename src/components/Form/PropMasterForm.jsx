5e356fc61c37823e19239bd4b90633a0fb8951b2
gaurang4453 <gaurang4453@gmail.com>
2025-02-18 17:07:21 +0530

    na

--- src/components/Form/PropMasterForm.jsx
+++ src/components/Form/PropMasterForm.jsx
@@ -82,4 +82,3 @@ function PropMasterForm() {
 }
 
 export default PropMasterForm;
-export default PropMasterForm;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,24 +1,14 @@
 import React from 'react'
 import Navbar from './Navbar/Navbar'
-<<<<<<< HEAD
 import Form from './Form/PropMasterForm'
 
-=======
-import { Route } from 'react-router-dom'
-import Routing from '../Routing'
->>>>>>> 0c905e39e348ec35d0bbae6bde7419ef7f28edc9
 
 const Home = () => {
   return (
     <>
-<<<<<<< HEAD
    
     <h1>hello</h1>
       {/* <Form /> */}
-=======
-    <Navbar/>
-    <Routing/>
->>>>>>> 0c905e39e348ec35d0bbae6bde7419ef7f28edc9
     </>
   )
 }
