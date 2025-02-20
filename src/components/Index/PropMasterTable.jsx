92976e4efab412709111a56abd664e6e360895f0
gaurang4453 <gaurang4453@gmail.com>
2025-02-20 16:55:29 +0530

    v

--- src/components/Index/PropMasterTable.jsx
+++ src/components/Index/PropMasterTable.jsx
@@ -2,9 +2,9 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import AxiosInstance from "../../AxiosInstance";
 import { Spinner, Table, Container, Button } from "react-bootstrap";
-import Footer from "../footer/Footer";
+
 import PropMasterForm from "../Form/PropMasterForm";
-import EditPropertyMst from "../../Edit/EditPropertyMst";   
+import EditPropertyMst from "../../Edit/EditPropertyMst";
 function PropMasterTable() {
   const [tableData, setTableData] = useState([]);
   const [loading, setLoading] = useState(true);
